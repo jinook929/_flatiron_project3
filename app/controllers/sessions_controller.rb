9cdfb0fb4cb27ab24a8cfd0b5fb442bb75ea39e6
jinook929 <jinook929@gmail.com>
2021-04-02 11:29:23 -0400

    clean up user index page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -43,7 +43,7 @@ class SessionsController < ApplicationController
       if @user.errors.empty?
         session[:user_id] = @user.id
         session[:tmp_password] = password if @user.password
-        redirect_to edit_password_path(@user), alert: "Set your password."
+        redirect_to edit_password_path(@user), alert: "Successfully logged in via Google. Now please set your password for this app."
       else
         @alert = "Google log in failed..."
         render 'new'
